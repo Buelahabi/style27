c788e188730b5eb7ab5649f2e398443f657cac49
Buelahabi <buelah007@gmail.com>
2022-03-24 16:38:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const project = ["Have project today 23rd march", "Have to submit tender tomorrow 24th march"]
+  const [copyproject,setproject]=useState(project)
+  const[userproject,setuserproject]=useState("")
+  const buttonstyle={
+    color:!userproject?"white":"blue"
+  }
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='container'>
+      <div className='row'>
+        <div className="col-12">
+          <div className="heading">
+            <h1>To Do Applications</h1>
+            </div>
+            <div classNmae="input-group mb-3">
+              <input placeholder='Enter your new project......' type="text" value={userproject} onChange={(Event)=>{
+               setuserproject(Event.target.value) 
+               console.log(userproject)
+              }} className="form-control"></input>
+              <button style={buttonstyle} disabled={!userproject} onClick={()=>{
+                setproject([...copyproject,userproject])
+                console.log(copyproject)
+                console.log(userproject)
+                setuserproject("")
+              }} className="btn bg-success btn-outline-secondary"type="button"id="button-addon2">Add new project</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    {copyproject.map((Event)=><Tdlist project={Event}/>)}
     </div>
   );
-}
+            }
+             function Tdlist({project}) {
+               const[currentdisplay,setdisplay]=useState(true);
+               //const styles={
+                 // display:currentdisplay?"block":"none",
+                 //}
+                 return(
+                   <div className='container'>
+                     <table class="table table-dark">
+                       <tbody>
+                         <tr>
+                           {currentdisplay?<td><bold>{project}</bold><button onClick={()=>setdisplay(false)} className='btn bg-danger'>Remove project</button></td>:""}
+
+                         </tr>
+                       </tbody>
+                     </table>
+                   </div>
+                 )
+               }
+             
 
 export default App;
